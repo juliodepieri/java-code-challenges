3e592258e75fff70d96fc376980fd81d2b0daf92
Julio de Pieri <juliodepieri@gmail.com>
2023-05-08 13:36:29 -0300

    add best time to buy and sell stock II solution

---
+++ src/main/java/br/com/jdp/array/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,44 @@
+package br.com.jdp.array;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+
+
+public class BestTimeToBuyAndSellStockII {
+
+    // Solution 1
+    // time complexity: O(N)
+    // space complexity: O(1)
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] < prices[i]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return totalProfit;
+    }
+
+    // Solution 2
+    // time complexity: O(N)
+    // space complexity: O(1)
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int maxProfitHere = 0;
+        int maxProfitSoFar = 0;
+        for (int i = 1; i < prices.length; i++) {
+            maxProfitHere = Math.max(0, maxProfitHere + prices[i] - prices[i - 1]);
+            maxProfitSoFar = Math.max(maxProfitSoFar, maxProfitHere);
+        }
+
+        return maxProfitSoFar;
+    }
+
+}
