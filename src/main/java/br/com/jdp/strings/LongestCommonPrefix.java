0c5a63690eff9d98bf3cf2ffabc4c737885bb25e
Julio de Pieri <juliodepieri@gmail.com>
2023-07-07 07:19:55 -0300

    add Longest common prefix solution

--- src/main/java/br/com/jdp/strings/LongestCommonPrefix.java
+++ src/main/java/br/com/jdp/strings/LongestCommonPrefix.java
@@ -1,2 +1,24 @@
-package br.com.jdp.strings;public class LongestCommonPrefix {
+package br.com.jdp.strings;
+
+/**
+ * time: O(n), where n is the sum of all characters in all strings.
+ * space: O(1)
+ * https://leetcode.com/explore/featured/card/top-interview-questions-easy/127/strings/887/
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
 }
--- src/test/java/br/com/jdp/strings/LongestCommonPrefixTest.java
+++ src/test/java/br/com/jdp/strings/LongestCommonPrefixTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.jdp.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LongestCommonPrefixTest {
-  
+
+    @Test
+    public void case1() {
+        String[] str = {"flower", "flow", "flight"};
+        assertThat(new LongestCommonPrefix().longestCommonPrefix(str)).isEqualTo("fl");
+    }
+
+    @Test
+    public void case2() {
+        String[] str = {"dog", "racecar", "car"};
+        assertThat(new LongestCommonPrefix().longestCommonPrefix(str)).isEmpty();
+    }
+
 }
